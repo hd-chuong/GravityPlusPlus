c9c397492c0232af8335b23f00e966b0288d513b
Chuong <chuong.git@gmail.com>
2021-01-14 12:56:09 +1100

    ADD: first solve cookie vs session problem

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
 
 var datasetRouter = require('./routes/datasetRouter');
 var dataRouter = require('./routes/dataRouter');
 var visRouter = require('./routes/visRouter');
 var intRouter = require('./routes/intRouter');
+var appRouter = require('./routes/appControl');
 var app = express();
 
 // view engine setup
@@ -15,11 +17,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(session({
+  secret: 'ABCD',
+  cookie: {maxAge: 100000},
+  saveUninitialized: false,
+  rolling: true,
+  resave: true, 
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// app API
+app.use('/app', appRouter);
 // dataset API
 app.use('/dataset', datasetRouter);
 // data API
