91ae862e2a14ee9eaa0be2d83fcc4d0705ab797d
Chuong <chuong.git@gmail.com>
2021-01-26 02:32:19 +1100

    ADD: Story model with publish feature

--- client/src/DemoPages/Story/PublishModal.js
+++ client/src/DemoPages/Story/PublishModal.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 const ModalExample = (props) => {
-  return (<Modal isOpen={props.open}>
-        <ModalBody>
-            Export scene to pdf
-        </ModalBody>
-      </Modal>
+  return (
   );
 }
 
