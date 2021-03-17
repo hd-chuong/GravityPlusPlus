6fb4ba69be19ab4706a77beb2f4cea43a1255d0a
Chuong <chuong.git@gmail.com>
2021-03-18 00:56:36 +1100

    bug fixed

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -8,7 +8,7 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
-const is139.59.103.42 = Boolean(
+const islocalhost= Boolean(
   window.location.hostname === '139.59.103.42' ||
     // [::1] is the IPv6 139.59.103.42 address.
     window.location.hostname === '[::1]' ||
